3ba598fdb52474813b1713c8d40c9e5de9f44965
Florent <florent.lagrede@gmail.com>
2020-06-29 09:07:24 +0200

    hide value box on empty item

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -31,9 +31,11 @@ const Item: React.FC<Props> = ({ name, icon, value, itemIndex }) => {
     >
       {isSelected && <TrianglesBox />}
       <img alt={name} src={icon} />
-      <div className="z-0 bg-black -mx-1 -my-1 text-sm text-white absolute bottom-0 right-0 border border-zelda-darkGray px-2">
-        {value}
-      </div>
+      {value && (
+        <div className="z-0 bg-black -mx-1 -my-1 text-sm text-white absolute bottom-0 right-0 border border-zelda-darkGray px-2">
+          {value}
+        </div>
+      )}
     </div>
   );
 };
